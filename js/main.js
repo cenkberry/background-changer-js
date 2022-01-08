09551289ad8e760f61865cd855f5afe977043aa2
Cenk Berry <83925796+cenkberry@users.noreply.github.com>
2022-01-08 17:16:33 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,35 @@
+const btn  = document.getElementById('btn');
+const body = document.body;
+
+btn.addEventListener("click" , function(){
+    
+    var letters = ["A","B","C","D","E","F"];
+    var result = [];
+    
+    var randomLetter1 = Math.floor( (Math.random()*5) );
+    var randomLetter2 = Math.floor( (Math.random()*5) );
+    var randomLetter3 = Math.floor( (Math.random()*5) );
+    
+    var randomNumber1  = Math.floor( (Math.random()*10) );
+    var randomNumber2  = Math.floor( (Math.random()*10) );
+    var randomNumber3  = Math.floor( (Math.random()*10) );
+    
+    
+    
+    var result = "#" +
+        letters[randomLetter1]+
+        randomNumber1+
+        letters[randomLetter2]+
+        randomNumber2+
+        letters[randomLetter3]+
+        randomNumber3
+                     ;
+    
+    
+    body.style.backgroundColor = " " + result;
+    console.log(result);
+    
+});
+
+
+
